053c3cf680429e856ceb809b2ecb99d5f90305e0
Henning <henning.loewe@hotmail.de>
2022-04-29 10:52:28 +0200

    feat(app): add test data

--- projects/splitwise-app/src/app/app.component.ts
+++ projects/splitwise-app/src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { GroupService, UserService } from 'splitwise';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'splitwise-app';
+export class AppComponent implements OnInit {
+    index = 1;
+    title = 'splitwise-app';
+
+    constructor(public groupService: GroupService, private dialog: MatDialog, private userService: UserService) {}
+
+    ngOnInit(): void {
+        this.addUsers();
+        this.addGroup();
+        this.addGroup();
+        this.addGroup();
+    }
+
+    addUsers(): void {
+        this.userService.createUser({
+            name: 'Henning'
+        });
+        this.userService.createUser({
+            name: 'Nils'
+        });
+    }
+
+    addGroup(): void {
+        this.groupService.createGroup({
+            name: 'Test' + this.index++,
+            userIds: [1, 2]
+        });
+    }
 }
